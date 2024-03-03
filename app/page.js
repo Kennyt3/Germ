7947a98b33f52b0d11dc91a1ed5dfd437fab7851
Mercy <kennyt341@gmail.com>
2024-03-03 13:55:21 +0100

    counter

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Register() {
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
   const [iban, setIban] = useState('')
-  const [superprovBerechtigt, setSuperprovBerechtigt] = useState('')
+  const [superprov, setSuperprov] = useState('')
   const [step, setStep] = useState(1)
   const nextStep = () => {
     setStep(step + 1)
@@ -49,9 +49,9 @@ export default function Register() {
       <h2>Worker Registration Form</h2>
       <form id='workerForm' onSubmit={handleClick} method='POST'>
         <ul id='progressbar'>
-          <li class='active'>Step 1</li>
-          <li>Step 2</li>
-          <li>Step 3</li>
+          <li className={`${step === 1 ? 'active' : ''}`}>Step 1</li>
+          <li className={`${step === 2 ? 'active' : ''}`}>Step 2</li>
+          <li className={`${step === 3 ? 'active' : ''}`}>Step 3</li>
         </ul>
         {step === 1 && (
           <div className='form-step'>
@@ -59,31 +59,31 @@ export default function Register() {
             <div class='form-group'>
               <label for='firstName'>First Name:</label>
               <input
+                required
                 type='text'
                 name='firstName'
                 value={vorname}
                 onChange={(e) => setVorname(e.target.value)}
-                required
               />
             </div>
             <div class='form-group'>
               <label for='lastName'>Last Name:</label>
               <input
+                required
                 type='text'
                 name='lastName'
                 onChange={(e) => setNachname(e.target.value)}
                 value={nachname}
-                required
               />
             </div>
             <div class='form-group'>
               <label for='email'>Email:</label>
               <input
+                required
                 type='email'
                 name='email'
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
-                required
               />
             </div>
             <div className='but-con1'>
@@ -99,6 +99,7 @@ export default function Register() {
             <div class='form-group'>
               <label for='referrerId'>Brought by Level 1 (ID):</label>
               <input
+                required
                 type='text'
                 name='referrerId'
                 onChange={(e) => setReferrer(e.target.value)}
@@ -106,8 +107,52 @@ export default function Register() {
               />
             </div>
             <div class='form-group'>
+              <label for='supervisor'>Supervisor:</label>
+              <input
+                required
+                type='text'
+                name='supervisor'
+                onChange={(e) => setSupervisor(e.target.value)}
+                value={supervisor}
+              />
+            </div>
+            <div class='form-group'>
+              <label for='superprov'>Superprov:</label>
+              <input
+                required
+                type='text'
+                name='superprov'
+                onChange={(e) => setSuperprov(e.target.value)}
+                value={superprov}
+              />
+            </div>
+            <div className='but-con'>
+              <button type='button' onClick={prevStep}>
+                Previous
+              </button>
+              <button type='button' onClick={nextStep}>
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+        {step === 3 && (
+          <div className='form-step'>
+            {' '}
+            <div class='form-group'>
+              <label for='iban'>IBAN:</label>
+              <input
+                required
+                type='text'
+                name='iban'
+                onChange={(e) => setIban(e.target.value)}
+                value={iban}
+              />
+            </div>
+            <div class='form-group'>
               <label for='city'>City:</label>
               <input
+                required
                 type='text'
                 name='city'
                 onChange={(e) => setCity(e.target.value)}
@@ -117,6 +162,7 @@ export default function Register() {
             <div class='form-group'>
               <label for='street'>Street:</label>
               <input
+                required
                 type='text'
                 name='street'
                 onChange={(e) => setStreet(e.target.value)}
